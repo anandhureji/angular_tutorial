0c16391b7df36fa0544f9f23b64cb96ee4cb518e
anandhu <anandhureji33@gmail.com>
2023-11-22 20:30:36 +0530

    ngOnit -> lifecycle hook and getting apppointment data from local storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import {OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+
+ 
 
   newAppointmentTitle : string = "";
   newAppointmentDate : Date = new Date();
 
   appointments : Appointment[] = []
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments")
+    this.appointments = savedAppointments? JSON.parse(savedAppointments): []
+    console.log("got loaded")
+  }
 
   addAppointment(){
     if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
@@ -33,7 +42,7 @@ export class AppointmentListComponent {
 
   deleteAppointment(index: number){
     this.appointments.splice(index,1)
-    localStorage.setItem("appointments",JSON.stringify(this.appointments))
+    localStorage.setItem("appointments",JSON.stringify(this.appointments))  // to store the apointment data in local storage
   }
 
 }
